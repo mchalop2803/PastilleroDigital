f98efb958a21ac2a100c082b0ec577a0aea8d679
mchalop2803 <mchalop2803@iescarrillo.es>
2026-01-16 12:54:44 +0100

    Vistas de de cita medica, familia añadidos

--- app/src/main/java/activities/AddCitaMedicaActivity.java
+++ app/src/main/java/activities/AddCitaMedicaActivity.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.pastillerodigital.R;
 
-public class SpashActivity extends AppCompatActivity {
+public class AddCitaMedicaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_spash);
+        setContentView(R.layout.activity_add_cita_medica);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
